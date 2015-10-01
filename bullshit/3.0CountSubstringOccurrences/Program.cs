9eba8c04f4ec21dae751d0cf57a7cee12e745bf7
broxigarthered <niki.dutskinov@hotmail.com>
2015-10-01 19:48:56 +0300

    Strings Homework

---
+++ bullshit/3.0CountSubstringOccurrences/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._0CountSubstringOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string searchedWord = Console.ReadLine();
+
+            int count = input.Select((c, i) => input.Substring(i))
+                .Count(x => x.StartsWith(searchedWord, StringComparison.CurrentCultureIgnoreCase));
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ bullshit/4.0TextFIlter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._0TextFIlter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] bannedWords = input.Split(new char[]
+                {',',' '},StringSplitOptions.RemoveEmptyEntries);
+
+            string secondInput = Console.ReadLine();
+
+            string replaced = secondInput.Replace(bannedWords[0],new string('*',bannedWords[0].Length))
+                .Replace(bannedWords[1], new string('*', bannedWords[1].Length));;
+
+            Console.WriteLine();
+            Console.WriteLine(replaced);
+
+        }
+    }
+}
